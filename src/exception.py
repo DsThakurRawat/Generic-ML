ad0101a8501af60464d13f828851675bb2239c38
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-01 16:13:22 +0530

    logging and exception handling

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys
+from types import ModuleType
+
+def error_message_detail(error: Exception, error_detail: ModuleType) -> str:
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+
+    error_message = (
+        f"Error occurred in python script name [{file_name}] "
+        f"line number [{line_number}] error message [{str(error)}]"
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error: Exception, error_detail: ModuleType):
+        self.error_message = error_message_detail(error, error_detail)
+        super().__init__(self.error_message)
+
+    def __str__(self) -> str:
+        return self.error_message
