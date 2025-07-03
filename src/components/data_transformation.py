769a929b9b65d92adae6d3dfb5633b5460e0bda2
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-03 17:54:20 +0530

    data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,62 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_ob_file_path = os.path.join("artifacts","preprocessor.pkl")
+
+class DaataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")), #for missing values we will use median
+                    ("scaler", StandardScaler()),#handling feature scaling
+                ]
+            )
+
+            cat_pipeline = Pipeline( #this is categorical pipeline 
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")), #for handling missing values of categorical feature
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler(with_mean=False)),
+                ]
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor = ColumnTransformer(  
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipelines", cat_pipeline, categorical_columns),
+                ]
+            )
+
+
+
+        except:
+            pass
+
