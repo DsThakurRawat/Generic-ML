db567f481698ef63e746f57cfee41bf286bbf052
DsThakurRawat <divyanshrawatofficial@gmail.com>
2025-07-05 17:59:31 +0530

    deployment phase

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify,render_template
 import numpy as np
 import pandas as pd
@@ -37,4 +36,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
        
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
